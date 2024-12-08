35649267d484a45c58a574a8726bdd7e1b6d36a8
vincentapa <v_apa@icloud.com>
2024-12-07 22:51:10 -0500

    Redo-9

---
+++ Service/Models/People.cs
@@ -0,0 +1,8 @@
+namespace TestSolution.Models;
+
+public class People
+{
+    public required int PersonId { get; set; }
+    public required string Name { get; set; }
+    public required DateTime PromotionDate { get; set; }
+}
